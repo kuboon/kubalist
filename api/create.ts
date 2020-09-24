3702afed0df2d9ce262cf12b5c6991782c16771e
ohkubo kohei <kuboon@trick-with.net>
2020-09-24 10:50:08 +0900

    upgrade npms & clean up records before a week

--- api/create.ts
+++ api/create.ts
@@ -1,18 +1,39 @@
-import {db} from './_lib/firestore'
-import {shuffle} from './_lib/shuffle'
-import {v1} from 'uuid'
-import {sha256} from 'hash.js'
-import { NowRequest, NowResponse } from '@now/node'
+import { db } from './_lib/firestore'
+import { shuffle } from './_lib/shuffle'
+import { v1 } from 'uuid'
+import { sha256 } from 'hash.js'
+import { NowRequest, NowResponse } from '@vercel/node'
 
+async function cleanUp () {
+  const at = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)
+  const batch = db.batch()
+  const snapshot = await db
+    .collection('rooms')
+    .where('createdAt', '<', at)
+    .get()
+  snapshot.docs.forEach((doc: any) => batch.delete(doc.ref))
+  return batch.commit()
+}
 export default async (req: NowRequest, res: NowResponse) => {
   res.setHeader('access-control-allow-origin', '*')
-  res.setHeader('access-control-allow-headers', 'origin,x-requested-with,content-type,accept')
+  res.setHeader(
+    'access-control-allow-headers',
+    'origin,x-requested-with,content-type,accept'
+  )
   const key = v1()
-  const id = sha256().update(key).digest('hex')
-  const cards = (req.query.cards as string).split(",")
+  const id = sha256()
+    .update(key)
+    .digest('hex')
+  const cards = (req.query.cards as string).split(',')
   shuffle(cards)
-  const numbers = Array(cards.length).fill(null).map((_,i)=>i+1)
+  const numbers = Array(cards.length)
+    .fill(null)
+    .map((_, i) => i + 1)
   shuffle(numbers)
-  await db.collection('rooms').doc(id).set({count: 0, cards, numbers, created_at: new Date})
-  res.json({key, id, cards, numbers})
+  await db
+    .collection('rooms')
+    .doc(id)
+    .set({ count: 0, cards, numbers, created_at: new Date() })
+  await cleanUp()
+  res.json({ key, id, cards, numbers })
 }
